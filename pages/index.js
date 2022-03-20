9b8bc5c88c4de976c5d555cde017c60b8e54ba93
Olatubora Felix Olawole <olatubora1996@gmail.com>
2022-03-20 18:53:32 +0100

    patagraph is add to the index page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ const index = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam a doloribus facere accusantium error unde obcaecati iste molestiae optio illum delectus, libero aperiam eligendi repellendus at sint distinctio. Accusamus, voluptatem.</p>
     </div>
   )
 }
